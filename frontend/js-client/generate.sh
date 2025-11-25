7c2a4a430da3d4153770a294e774b79f160df463
Tilman Holube <tilman@holube.de>
2025-11-25 16:26:45 +0100

    make volume path absolute
    Generating the js-client requires the path of the host volumes to be absoulte. Don't ask why, this is how github wants it to be...

--- frontend/js-client/generate.sh
+++ frontend/js-client/generate.sh
@@ -4,7 +4,7 @@ GID=$(id -g)
 docker run --rm \
   -u "${UID}":"${GID}" \
   -v "${PWD}":/local \
-  -v ../../openapi/openapi.yaml:/input/openapi.yaml \
+  -v "${PWD}/../../openapi/openapi.yaml":/input/openapi.yaml \
    openapitools/openapi-generator-cli:v7.13.0 generate \
   -i /input/openapi.yaml \
   -g javascript \
