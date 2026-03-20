241edf3f21a981b73a2ee7073b8bffeb5bfb3ce9
Jonas Pohl <git@jaypi4c.de>
2026-03-20 09:27:58 +0100

    Adding more explainable text to install sections

--- README.md
+++ README.md
@@ -4,6 +4,11 @@ Th1nk is a project which aims to simplify the process of extracting and visualiz
 
 ## Installation
 
+### Prerequisites
+
+- Git
+- Docker
+
 **TL;DR**:
 ```bash
 git clone https://github.com/uol-esis/Th1nk.git
@@ -17,6 +22,8 @@ To start Th1nk, simply clone the repo and run the following command:
 ```bash
 ./install.sh
 ```
+
+Now follow the instructions in the terminal to complete the setup.
 
 > [!CAUTION]
 > This will start Th1nk in an unsecure way. Please consult the wiki for a properly secured deployment.
--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 docker compose -f backend/docker/docker-compose.yaml up -d frontend db metabase
 
 echo "Please configure Metabase at http://localhost:3000. (Further information can be found in the documentation.)"
+echo "Database configuration: https://www.metabase.com/docs/v0.57/databases/connections/postgresql.html. The DB credentials can be found in the backend/docker/docker-compose.yaml file."
+echo "Type: postgres, Username: example, Password: example, Host: db, Port: 5432, Database name: th1nk, Schemas: all except th1-internal"
+echo "Obtaining API key with Administrator permissions: https://www.metabase.com/docs/v0.57/people-and-groups/api-keys"
+echo "Provide the obtained API key as an environment variable named METABASE_API_KEY."
 read -p "Press enter to continue after configuration"
 
 docker compose -f backend/docker/docker-compose.yaml up -d backend
